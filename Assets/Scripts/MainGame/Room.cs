cdeddb2b98f90f8864ba8e4662e72fe77a535d68
Luqmus <rjcnz1007@yandex.ru>
2023-03-03 17:40:42 +0400

    1

--- Assets/Scripts/MainGame/Room.cs
+++ Assets/Scripts/MainGame/Room.cs
@@ -11,26 +11,16 @@ public class Room : MonoBehaviour
 
     protected virtual void OnEnable()
     {
-        Player.GetComponent<NavMeshAgent>().enabled = false;
-        Player.position = _enterPortal.position;
-        Player.GetComponent<NavMeshAgent>().enabled = true;
+        TeleportPlayer();
 
         foreach (var portal in _portals)
             portal.gameObject.SetActive(false);
     }
 
-    protected void GeneratePortals()
+    private void TeleportPlayer()
     {
-        int counter = 0;
-
-        foreach (var portal in _portals)
-        {
-            int activePortal = Random.Range(0, 2);
-            counter += activePortal;
-            portal.gameObject.SetActive(System.Convert.ToBoolean(activePortal));
-        }
-
-        if (counter == 0)
-            _portals[Random.Range(0, _portals.Length)].gameObject.SetActive(true);
+        Player.GetComponent<NavMeshAgent>().enabled = false;
+        Player.position = _enterPortal.position;
+        Player.GetComponent<NavMeshAgent>().enabled = true;
     }
 }
---
+++ Assets/Scripts/MiniGames/ImpSlice/ImpSlice_Player.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpSlice_Player : MonoBehaviour
+{
+
+}
